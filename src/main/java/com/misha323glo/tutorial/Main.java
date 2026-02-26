95a4bdf4e6b06fcbc944ff31b85ec22d74506b58
Alexey Orlyuk <alex323glo@gmail.com>
2026-02-26 23:13:46 +0200

    Add new greeting to Main class

--- src/main/java/com/misha323glo/tutorial/Main.java
+++ src/main/java/com/misha323glo/tutorial/Main.java
@@ -3,5 +3,6 @@ package com.misha323glo.tutorial;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World! I am Misha.");
+        System.out.println("Hello, World! I am Alex ;)")
     }
 }
